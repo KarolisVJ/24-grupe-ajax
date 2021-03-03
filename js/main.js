b5060b04a216552ca33164ce391a512c7b85331d
Karolis Jachimavičius <76951309+KarolisVJ@users.noreply.github.com>
2021-03-03 18:26:54 +0200

    diff response

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 function ajax() {
     const xhttp = new XMLHttpRequest();
+    console.log(xhttp);
     xhttp.onreadystatechange = function () {
         console.log(this.readyState);
 
         if (this.readyState === 4 && this.status === 200) {
             const response = this.responseText;
-
+            console.log(this.responseText);
             const obj = JSON.parse(response);
 
             console.log(obj);
         }
     };
-    xhttp.open("GET", "https://front-end-by-rimantas.github.io/24-grupe-ajax/api/rikis.json", true);
+    xhttp.open("GET", '../api/response.json', true);
     xhttp.send();
 }
 
